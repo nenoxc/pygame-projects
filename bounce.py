fa54bc71d9b720ca37b85ecdd31dc78e00b39c58
nenoxc <59512323+nenoxc@users.noreply.github.com>
2020-12-21 23:15:12 +0100

    Add files via upload

--- bounce.py
+++ bounce.py
@@ -1,7 +1,9 @@
 import pygame
+from pygame import gfxdraw
 import math
 import random
 import time
+import PIL
  
 pygame.init()
 w = 1280
@@ -11,22 +13,47 @@ f = pygame.font.SysFont('Calibri', 30)
 pygame.display.set_caption("Physiksimulation")
 Objects = []
 BorderLines = []
+particles = []
 timer = 0
 interval = 0.01
 bounceStrength = 0.7
 run = True
 pos = [0,0]
-a = -0.981
+a = -9.81
 clock = pygame.time.Clock()
 
-def getDist(x0,y0,x1,y1):
-	return abs(math.sqrt((x0-x1)**2+(y0-y1)**2))
+def draw_circle(surface, x, y, radius, color):
+	gfxdraw.aacircle(surface, x, h-y, radius, color)
+	gfxdraw.filled_circle(surface, x, h-y, radius, color)
+def getDist(x0,y0,x1,y1):return abs(math.sqrt((x0-x1)**2+(y0-y1)**2))
+def subtract(arr1,arr2):return [e - arr2[c] for c, e in enumerate(arr1)]
+
+class Particle:
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+		self.color = [200,200,200]
+		self.radius = random.randint(2,6)
+		self.velocity = [random.randint(-2,2),random.randint(-2,2)]
+	def draw(self):
+		self.fly()
+		draw_circle(win, int(self.x+self.radius*0.25),int(self.y+self.radius*0.25),int(self.radius), subtract(self.color, [20,20,20]))
+		draw_circle(win, int(self.x),int(self.y), int(self.radius), self.color)
+	def fly(self):
+		self.x +=self.velocity[0]
+		self.y +=self.velocity[1]
+		self.radius -= 0.1
+		if(self.radius<=0):
+			self.remove()
+	def remove(self):
+		particles.pop(particles.index(self))
+
 
 class Object:
 	def __init__(self,x,y,mass,color,radius, startspeed=[0,0]):
 		self.x = x
 		self.y = y
-		self.color = [random.randint(0,255),random.randint(0,255),random.randint(0,255)]
+		self.color = [random.randint(200,255),0,0]
 		self.velocity = startspeed
 		self.r = 580
 		self.mass = mass
@@ -34,12 +61,13 @@ class Object:
 	def move(self):
 		self.x += self.velocity[0]
 		self.y += self.velocity[1]
-		v = self.velocity[1] + a * interval
-		self.velocity[1] = v
+		self.velocity[1] += a * interval
 	def draw(self):
 		self.checkColliders()
 		self.move()
-		pygame.draw.circle(win, self.color, (int(self.x),h-int(self.y)), self.radius)
+		draw_circle(win,int(self.x)+2,int(self.y)-2, self.radius+1, [20,20,20])
+		draw_circle(win,int(self.x),int(self.y), self.radius, self.color)
+
 	def addForce(self, dir, n):
 		self.velocity = [self.velocity[0] + math.cos(dir)*n,self.velocity[1] + math.sin(dir)*n]
 	def checkColliders(self):
@@ -56,6 +84,8 @@ class Object:
 		self.velocity = [math.cos(angle)*ov,math.sin(angle)*ov]
 		self.x += self.velocity[0]
 		self.y += self.velocity[1]
+		if(self.velocity[0]+self.velocity[1]>1):
+			for i in range(5): particles.append(Particle(self.x,self.y))
 class Border:
 	def __init__(self, p1, p2, color):
 		if(p1[1]>=p2[1]):
@@ -69,17 +99,18 @@ class Border:
 		dist1 = getDist(circle[0], circle[1],self.p1[0],self.p1[1])
 		dist2 = getDist(circle[0], circle[1],self.p2[0],self.p2[1])
 		length = getDist(self.p1[0],self.p1[1],self.p2[0],self.p2[1])
-		if(int((dist1+dist2)-length)==0):
+		if(abs(int((dist1+dist2)-length))==0):
 			return True
 		return False
 	def draw(self):
+		pygame.draw.line(win, [20,20,20], [self.p1[0]+5, 720-self.p1[1]+5], [self.p2[0]+5, 720-self.p2[1]+5], 3)
 		pygame.draw.line(win, self.color, [self.p1[0], 720-self.p1[1]], [self.p2[0], 720-self.p2[1]], 3)
 
-BorderLines.append(Border([0,20], [1280,20], [255,255,255]))
+BorderLines.append(Border([0,20], [1280,20], [200,200,200]))
 mouseSpeed = [0,0]
 oldMousepos = [0,0]
 while run:
-	clock.tick(165)
+	clock.tick(60)
 	mb = pygame.mouse.get_pressed()
 	k = pygame.key.get_pressed()
 	mouse = pygame.mouse.get_pos()
@@ -91,17 +122,18 @@ while run:
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			mb = pygame.mouse.get_pressed()
 			if(mb[0]): pos = [mouse[0], h-mouse[1]]
-			if(mb[2]): BorderLines.append(Border(pos, [mouse[0], h-mouse[1]], [255,255,255]))
+			if(mb[1]):Objects.append(Object(mouse[0],720-mouse[1], 10, [255,0,0], 5, startspeed=mouseSpeed))
+			if(mb[2]): BorderLines.append(Border(pos, [mouse[0], h-mouse[1]], [200,200,200]))
 	win.fill((0,0,0))
-	if(mb[1]):Objects.append(Object(mouse[0],720-mouse[1], 10, [255,0,0], 5, startspeed=mouseSpeed))
 	mouse = pygame.mouse.get_pos()
 	timer+=interval
 	win.blit(f.render(str(int(clock.get_fps())), True, [255,255,255]), (50,50))
 	win.blit(f.render(str(len(Objects)), True, [255,255,255]), (200,50))
-	for o1 in Objects:
-		o1.draw()
 	for border in BorderLines:
 		border.draw()
-	#win.blit(f.render(str(o2.velocity), True, (255, 255, 255)), (10,10))
+	for o1 in Objects:
+		o1.draw()
+	for p in particles:
+		p.draw()
 	pygame.display.update()
 pygame.quit()
--- orbit.py
+++ orbit.py
@@ -4,7 +4,9 @@ import random
 import time
  
 pygame.init()
-win = pygame.display.set_mode((1280,720))
+w = 1920
+h = 1080
+win = pygame.display.set_mode((w,h))
 f = pygame.font.SysFont('Calibri', 30)
 pygame.display.set_caption("Physiksimulation")
 Objects = []
@@ -24,7 +26,7 @@ class Object:
 		self.x = x
 		self.y = y
 		self.color = color
-		self.velocity = [0,0]
+		self.velocity = [0,-1]
 		self.r = 580
 		self.mass = mass
 		self.radius = radius
@@ -32,21 +34,17 @@ class Object:
 	def move(self):
 		self.x += self.velocity[0]
 		self.y += self.velocity[1]
+		#if(getDist(self.x,self.y,Objects[0].x,Objects[0].y)<=15 and self!=Objects[0]): Objects.pop(Objects.index(self))
 		return
-		a = -9.81
-		r = self.y + 0.5 * (self.velocity[1]+self.velocity[1])*interval
-		v = self.velocity[1] + a * interval
-		self.y = r
-		self.velocity[1] = v
 	def draw(self):
-		pygame.draw.circle(win, self.color, (int(self.x),720-int(self.y)), self.radius)
+		pygame.draw.circle(win, self.color, (int(self.x),h-int(self.y)), self.radius)
 		self.move()
 	def addForce(self, dir, n):
 		self.velocity = [self.velocity[0] + math.cos(dir)*n,self.velocity[1] + math.sin(dir)*n]
 
-Objects.append(Object(620,360, 1000000, [255,0,0], 6))
-Objects.append(Object(260,360, 1000000/1000, [255,0,0], 1))
-Objects[1].velocity = [0,-0.015]
+Objects.append(Object(w/2,h/2, 1000000000, [255,255,0], 10))
+for i in range(10000): Objects.append(Object(random.randint(0, w),random.randint(0, h), 10000000/1000, [255,0,0], 1))
+Objects[0].velocity = [0,0]
 
 
 while run:
@@ -54,21 +52,24 @@ while run:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			Objects[0].x, Objects[0].y = pygame.mouse.get_pos()[0], 720-pygame.mouse.get_pos()[1]
+		#if event.type == pygame.MOUSEBUTTONDOWN:
+		#Objects[0].x, Objects[0].y = pygame.mouse.get_pos()[0], 720-pygame.mouse.get_pos()[1]
 	win.fill((0,0,0))
 	mouse = pygame.mouse.get_pos()
 	k = pygame.key.get_pressed()
-	if(k[pygame.K_d]): Objects[0].velocity[0]+=0.005
-	if(k[pygame.K_a]): Objects[0].velocity[0]-=0.005
+	if(k[pygame.K_w]): Objects[0].y+=1
+	if(k[pygame.K_a]): Objects[0].x-=1
+	if(k[pygame.K_s]): Objects[0].y-=1
+	if(k[pygame.K_d]): Objects[0].x+=1
 	mouse = pygame.mouse.get_pos()
 	timer+=interval
 	for o1 in Objects:
 		o1.draw()
 	o1 = Objects[0]
-	o2 = Objects[1]
-	force = getGForce(o1.mass, o2.mass, getDist(o1.x,o1.y,o2.x,o2.y))
-	o2.addForce(math.atan2(o1.y-o2.y,o1.x-o2.x), force)
+	for o in Objects:
+		if(o!=o1):
+			force = getGForce(o1.mass, o.mass, getDist(o1.x,o1.y,o.x,o.y))
+			o.addForce(math.atan2(o1.y-o.y,o1.x-o.x), force)
 	#win.blit(f.render(str(o2.velocity), True, (255, 255, 255)), (10,10))
 	pygame.display.update()
 pygame.quit()
